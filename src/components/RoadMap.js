e431df5ad1eef740e847447cd99afcd77a20c3ae
Ziyad Eslam <ziyadeslam@hotmail.com>
2024-10-17 04:29:34 +0300

    rendering tweaks

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Stage, Layer, Line } from "react-konva";
+import { Stage, Layer, Circle, Line } from "react-konva";
 import fetchRoads from "../utils/fetchRoads";
 
 const RoadMap = ({ lat, lng }) => {
@@ -50,7 +50,7 @@ const RoadMap = ({ lat, lng }) => {
   };
 
   const calculateOffset = (x1, y1, x2, y2, laneIndex, totalLanes) => {
-    const laneWidth = window.innerWidth * 0.00205;
+    const laneWidth = window.innerWidth * 0.0017;
     const angle = Math.atan2(y2 - y1, x2 - x1);
     const perpendicularAngle = angle + Math.PI / 2;
     const offset = ((laneIndex - (totalLanes - 1) / 2) * laneWidth);
@@ -76,6 +76,16 @@ const RoadMap = ({ lat, lng }) => {
       style={{ backgroundColor: "#f0f0f0" }}
     >
       <Layer scaleY={-1} y={window.innerWidth * 0.4}>
+        {Object.entries(roads).map((node) => (
+          <Circle
+            key={node[1].id}
+            x={node[1].lng * window.innerWidth * 0.6}
+            y={node[1].lat * window.innerWidth * 0.4}
+            radius={window.innerWidth * 0.000825}
+            fill="black"
+          />
+        ))}
+
         {Object.entries(roads).map((node) =>
           node[1].to.map((connection) => {
             const targetNode = roads[connection.id];
@@ -104,7 +114,7 @@ const RoadMap = ({ lat, lng }) => {
                     targetNode.lat * window.innerWidth * 0.4 + offsetY,
                   ]}
                   stroke="black"
-                  strokeWidth={window.innerWidth * 0.002}
+                  strokeWidth={window.innerWidth * 0.00165}
                 />
               );
             }
